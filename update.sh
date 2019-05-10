45fdad0ae8ba6bb60d3112464a94c0edcc8f44f4
Nicolas Stalder <n@stalder.io>
2019-05-11 01:53:32 +0200

    Make compatible with stm32l4 crate

--- update.sh
+++ update.sh
@@ -4,13 +4,13 @@ set -e
 
 # NOTE: Last executed using Rust 1.34.0
 
-cargo install --force --version 0.14.0 svd2rust
-cargo install --force --version 0.6.0  form
-rustup component add rustfmt
+# cargo install --force --version 0.14.0 svd2rust
+# cargo install --force --version 0.6.0  form
+# rustup component add rustfmt
 
 rm -rf src
 mkdir src
-svd2rust -i ./stm32l4x2.svd
+svd2rust -i ./stm32l4x2.svd.patched
 form -i lib.rs -o src
 rm lib.rs
 cargo fmt
